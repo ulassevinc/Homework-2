d17d7a7a50fd26784ddaadbead6a8e40a8e494f0
ulassevinc <80682861+ulassevinc@users.noreply.github.com>
2021-04-01 17:38:36 +0300

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,21 @@
+Chatbot = 'Hi i am chatbot.Please state your name.'
+print('Chatbot:', Chatbot)
+User ='Bora Güngören'
+print('User:', User)
+Chatbot = 'Thank you Bora Güngören. How can i help you?'
+print('Chatbot:', Chatbot)
+User = 'Account Balance'
+print('User:', User)
+Chatbot ='Searching for Account Balance...'
+print('Chatbot:', Chatbot)
+
+
+
+
+
+
+
+
+
+
+
